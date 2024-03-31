e4e807b5b8f0f84012f2623e808ae84cd135f277
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-03-31 03:32:16 +0300

    Add project details

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+
+http
+  .createServer((req, res) => {
+    let filePath = "./";
+    switch (req.url) {
+      case "/":
+        filePath += "index.html";
+        break;
+      case "/about":
+        filePath += "about.html";
+        break;
+      case "/contact-me":
+        filePath += "contact-me.html";
+        break;
+      default:
+        filePath += "404.html";
+        break;
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        fs.readFile("./404.html", (err, content) => {
+          res.end(data, "utf-8");
+        });
+      } else {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.end(data);
+      }
+    });
+  })
+  .listen(8080);
