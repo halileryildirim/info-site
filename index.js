66781b69cd5af5e1cd28d002fba253ff272a8ecb
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-04-01 01:30:07 +0300

    Refactor index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const http = require("http");
-const fs = require("fs");
+const http = require("node:http");
+const fs = require("node:fs");
 
 http
   .createServer((req, res) => {
@@ -8,12 +8,15 @@ http
       case "/":
         filePath += "index.html";
         break;
+
       case "/about":
         filePath += "about.html";
         break;
+
       case "/contact-me":
         filePath += "contact-me.html";
         break;
+
       default:
         filePath += "404.html";
         break;
@@ -22,12 +25,12 @@ http
     fs.readFile(filePath, (err, data) => {
       if (err) {
         res.writeHead(404, { "Content-Type": "text/html" });
-        fs.readFile("./404.html", (err, content) => {
+        fs.readFile("404.html", (err, data) => {
           res.end(data, "utf-8");
         });
       } else {
-        res.writeHead(200, { "Content-type": "text/html" });
-        res.end(data);
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data, "utf-8");
       }
     });
   })
